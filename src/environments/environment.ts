146d9eee0d84a87b7128608db0156cbc6f4f55bc
s000332 <85288592+s000332@users.noreply.github.com>
2021-06-28 08:55:39 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCfjmVfnSqaKp08BkKpJupP524cRNuVhhM",
-    authDomain: "ficha-aad12.firebaseapp.com",
-    databaseURL: "https://ficha-aad12-default-rtdb.firebaseio.com",
-    projectId: "ficha-aad12",
-    storageBucket: "ficha-aad12.appspot.com",
-    messagingSenderId: "869911060602",
-    appId: "1:869911060602:web:6a58f13474cb6ca9e41bc3",
-    measurementId: "G-V1T1YPGL2M"
+    apiKey: "PRUEBAS",
+    authDomain: "PRUEBAS",
+    databaseURL: "PRUEBAS",
+    projectId: "PRUEBAS",
+    storageBucket: "PRUEBAS",
+    messagingSenderId: "PRUEBAS",
+    appId: "PRUEBAS",
+    measurementId: "PRUEBAS"
   }
 };
 
